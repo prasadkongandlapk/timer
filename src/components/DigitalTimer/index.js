1e713488611495ff42622fbf6ec6d9c0cc09c8d1
prasadkongandlapk <prasadkongandlapk@gmail.com>
2023-07-25 06:01:53 +0000

    digital

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -1,61 +1,152 @@
 import './index.css'
 import {Component} from 'react'
+import {PassThrough} from 'stream'
 
 const Timer = props => {
-  const {count, date} = props
+  const {count, isTimeStarted, minCount} = props
+
+  const secCount = count === 60 ? '00' : count
+
+  const seconds = count < 10 ? `0${secCount}` : count
+
+  const minutes = minCount < 10 ? `0${minCount}` : minCount
+
+  const pauseBtn = isTimeStarted ? (
+    <p className="bhdhfhsdfhs">Running</p>
+  ) : (
+    <p className="bhdhfhsdfhs">Paused</p>
+  )
 
   return (
     <div className="djladsj">
       <div className="timer-card">
-        <p>{date}</p>
+        <div className="time-card">
+          <h1 className="ppppp">{minutes}</h1>
+          <h1 className="ppppp">:</h1>
+          <h1 className="ppppp">{count === 60 ? secCount : seconds}</h1>
+        </div>
+        {pauseBtn}
       </div>
     </div>
   )
 }
 
 class DigitalTimer extends Component {
-  state = {date: new Date(), isTimeStarted: true, count: 0}
+  state = {btnCount: 0, minCount: 25, isTimeStarted: false, count: 60}
+
+  tick = () => {
+    const {count, minCount} = this.state
+    if (count > 0) {
+      this.setState(prevState => ({
+        count: prevState.count - 1,
+      }))
+    } else if (minCount > 0 && count === 0) {
+      this.setState(prevState => ({
+        minCount: prevState.minCount - 1,
+        count: 60,
+      }))
+    }
+  }
 
   onClickStartBtn = () => {
     this.setState(preState => ({
       isTimeStarted: !preState.isTimeStarted,
     }))
+    const intervalId = setInterval(this.tick, 1000)
+    return intervalId
+  }
+
+  onClickPauseBtn = () => {
+    this.setState(preState => ({
+      isTimeStarted: !preState.isTimeStarted,
+    }))
+  }
+
+  onClickResetBtn = () => {
+    clearInterval(this.intervalId)
+
+    this.setState({
+      isTimeStarted: false,
+      count: 60,
+      minCount: 25,
+    })
   }
 
   decreaseBtn = () => {
-    this.setState(prevState => ({count: prevState.count - 1}))
+    const {btnCount, minCount} = this.state
+    if (btnCount > 0) {
+      this.setState(prevState => ({
+        btnCount: prevState.minCount - 1,
+        minCount: prevState.minCount - 1,
+      }))
+    }
   }
 
   increaseBtn = () => {
-    this.setState(prevState => ({count: prevState.count + 1}))
+    const {btnCount, minCount} = this.state
+
+    this.setState(prevState => ({
+      btnCount: prevState.minCount + 1,
+      minCount: prevState.minCount + 1,
+    }))
   }
 
   render() {
-    const {isTimeStarted, count, date} = this.state
+    const {isTimeStarted, btnCount, minCount, count} = this.state
 
-    const startButton = isTimeStarted
-      ? 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png'
-      : 'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
+    const startButton = isTimeStarted ? (
+      <div className="start-btn-card">
+        <button
+          type="button"
+          className="start-btn"
+          onClick={this.onClickPauseBtn}
+        >
+          <img
+            className="start-img"
+            src="https://assets.ccbp.in/frontend/react-js/pause-icon-img.png"
+            alt="pause icon"
+          />
+          Pause
+        </button>
+      </div>
+    ) : (
+      <div className="start-btn-card">
+        <button
+          type="button"
+          className="start-btn"
+          onClick={this.onClickStartBtn}
+        >
+          <img
+            className="start-img"
+            src="https://assets.ccbp.in/frontend/react-js/play-icon-img.png"
+            alt="play icon"
+          />
+          Start
+        </button>
+      </div>
+    )
+
     return (
       <div className="bg">
         <h1>Digital Timer</h1>
-        <Timer count={count} date={date} />
+        <Timer
+          count={count}
+          btnCount={btnCount}
+          minCount={minCount}
+          isTimeStarted={isTimeStarted}
+        />
+
         <div className="align-right">
           <div className="btns-card">
-            <div className="start-btn-card">
-              <button
-                type="button"
-                className="start-btn"
-                onClick={this.onClickStartBtn}
-              >
-                <img className="start-img" src={startButton} />
-                {isTimeStarted ? <p>Start</p> : <p>Pause</p>}
-              </button>
-            </div>
+            {startButton}
             <div className="reset-btn-card">
-              <button type="button" className="reset-btn">
+              <button
+                onClick={this.onClickResetBtn}
+                type="button"
+                className="reset-btn"
+              >
                 <img
-                  className="start-img"
+                  className="reset-img"
                   src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
                   alt="reset icon"
                 />
@@ -73,7 +164,7 @@ class DigitalTimer extends Component {
             >
               -
             </button>
-            <p className="p-count">{count}</p>
+            <p className="p-count">{minCount}</p>
             <button
               onClick={this.increaseBtn}
               className="button-count"
